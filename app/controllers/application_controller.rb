5f855968312c39a1bb16f54122118930882c930a
Ana Harris <vana1990@gmail.com>
2018-12-31 15:37:21 -0500

    auth done, key hidden, needs to be tested with frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def encode_token(payload)
-    JWT.encode(payload, 'my_s3cr3t')
+    JWT.encode(payload, ENV['SECRET_KEY'])
   end
 
   def auth_header
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
     if auth_header
       token = auth_header.split(' ')[1]
       begin
-        JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+        JWT.decode(token, ENV['SECRET_KEY'], true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,13 @@ module DistrictTree
     config.api_only = true
   end
 end
+
+config_files = ['secrets.yml']
+
+config_files.each do |file_name|
+    file_path = File.join(Rails.root, 'config', file_name)
+    config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+    config_keys.each do |k,v|
+      ENV[k.upcase] ||= v
+    end
+end
