174ea0319d2ced4f1c2713231acbeb3793302055
Ana Harris <vana1990@gmail.com>
2019-03-13 20:39:22 -0400

    stop reading secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -36,10 +36,10 @@ end
 
 config_files = ['secrets.yml']
 
-config_files.each do |file_name|
-    file_path = File.join(Rails.root, 'config', file_name)
-    config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
-    config_keys.each do |k,v|
-      ENV[k.upcase] ||= v
-    end
-end
+#config_files.each do |file_name|
+#    file_path = File.join(Rails.root, 'config', file_name)
+#    config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+#    config_keys.each do |k,v|
+#      ENV[k.upcase] ||= v
+#    end
+#end
